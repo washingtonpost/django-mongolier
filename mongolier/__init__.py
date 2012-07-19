8b999b6d7a0a33edd5a00aebac379ec2f0550d7b
Jason Bartz <jrbartz@gmail.com>
2012-07-19 13:51:55 -0400

    popped version up 1

--- mongolier/__init__.py
+++ mongolier/__init__.py
@@ -4,10 +4,10 @@
 # django-mongolier
 ##
 __title__ = 'mongolier'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Jason Bartz & Jeremy Bowers'
 __license__ = 'MIT'
-__author_email__ = ['bartzj@washpost.com','bowersj@washpost.com']
+__author_email__ = ['bartzj@washpost.com', 'bowersj@washpost.com']
 
 from mongolier.db import MongoConnection, PersistentConnection
 from mongolier.utils.convert import ConvertDecimal
